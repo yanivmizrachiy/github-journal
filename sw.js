203c43bb21f08e68fdfe143e433c6a6263a37e3c
Yaniv Mizrachi <yanivmiz77@gmail.com>
2026-04-07 14:00:48 +0300

    Force clear old PWA cache and reload homepage clean

--- sw.js
+++ sw.js
@@ -1,31 +1,3 @@
-const CACHE = 'github-journal-v1';
-const ASSETS = [
-  '/github-journal/',
-  '/github-journal/index.html',
-  '/github-journal/editor.html',
-  '/github-journal/manifest.json',
-  '/github-journal/icon.svg'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return resp;
-    }).catch(() => caches.match('/github-journal/editor.html')))
-  );
-});
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.map(k=>caches.delete(k)));await self.clients.claim();})()));
+self.addEventListener('fetch',()=>{});
